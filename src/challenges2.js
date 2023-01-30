8feaada2f89108f4781fb9105b3a2fc19e39541e
Gilliady <gilliadyazevedo@gmail.com>
2023-01-30 20:01:11 -0300

    corrigido esLint request

--- src/challenges2.js
+++ src/challenges2.js
@@ -44,8 +44,11 @@ const generatePhoneNumber = (array) => {
   const numberRepeat = verifyNumbersRepeat(counters, true);
   const arrayValues = verifyNumbersValue(array, true);
   let canGenerate = numberRepeat && arrayValues && array.length === 11;
+  let firstSection = `(${array[0]}${array[1]})`;
+  let secondSection = `${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`;
+  const lastSection = `${array[7]}${array[8]}${array[9]}${array[10]}`;
   if (canGenerate) {
-    message = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+    message = `${firstSection} ${secondSection}-${lastSection}`;
   } else {
     message = cantGenerate(array, numberRepeat, arrayValues);
   }
